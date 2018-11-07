ce1091dc56d26278d26bc5d94300ed12ae1b0186
JiangShaoYin <tooo_cold@163.com>
2018-11-07 20:17:36 +0800

    demom

--- Mnist/my_mnist/app.py
+++ Mnist/my_mnist/app.py
@@ -7,26 +7,31 @@
  
 import tensorflow as tf
 import numpy as np
+import mnist_forward
+import mnist_backward
 from PIL import Image
 
 def pre_pic(picName):
     print picName
-    img = Image.open(picName)
-    reIm = img.resizei((28,28),Image.ANTIALIAS)
+    img = Image.open("./pic/1.png")
+#    img = Image.open(picName)
+    print '...........................................'
+    reIm = img.resize((28,28),Image.ANTIALIAS)
     #transform the picture to greyscale(0 stands for pure black,255 stands for pure white
     #,the integers from 1~254 stand for grey level)
     im_array = np.array(reIm.convert('L'))
+#    print im_array
     #set threshold to distinguish between balck and white,if the vale > threshold,treat it as pure balck(255)
-    threshold = 50
+    threshold = 100
     for i in range(28):
-        print picName
         for j in range(28):
             #invert the image as the input images are just the opposite of images which the neural network training with
-            im_array[i][j] = 255 - im_arr[i][j]
+            im_array[i][j] = 255 - im_array[i][j]
             if (im_array[i][j] < threshold):
                 im_array[i][j] = 0
             else:
                 im_array[i][j] = 255
+#    print im_array
     #reshape the picture from 28*28 to 1*784
     reshaped_array = im_array.reshape([1,784])
     #cast the value from int to float
@@ -34,14 +39,39 @@ def pre_pic(picName):
     #transfer the value from 0/255,to 0/1
     img_ready = np.multiply(reshaped_array, 1.0/255)
 
+def restore_model(testPicArr):
+    with tf.Graph().as_default() as g:
+        #compute forwa
+        x = tf.placeholder(tf.float32, [None, mnist_forward.INPUT_NODE])
+        y = mnist_forward.forward(x, None)
+        #define the predicted value
+        predictValue = tf.argmax(y,1)
+        
+        variable_average = tf.train.ExponentialMovingAverage(mnist_backward.MOVING_AVERAGE_DECAY)
+        variable_to_restore = variable_average.variables_to_restore()
+        saver = tf.train.Saver(variable_to_restore)
+
+        with tf.Session() as sess:
+            ckpt = tf.train.get_checkpoint_state(mnist_backward.MODEL_SAVE_PATH)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                predictValue = sess.run(predictValue, feed_dict={x:testPicArr})
+                return predictValue
+            else:
+                print "no checkpoint file found"
+                return -1
 
 
 def application():
     testNum = input("input the number of test pictures:")
-#    for i in range(testNum):
+    for i in range(testNum):
         #testPic = raw_input("./pic/" + "%d.png"(i))
-    testPic = raw_input("./pic/1.png")
-    pre_pic(testPic)
+        testPic = raw_input("./pic/2.png")
+#    testPic = raw_input("./pic/1.png")
+        testPicArr = pre_pic(testPic)
+        predictValue = restore_model(testPic)
+        print "the predict number is :"%predictValue
+
     print testPic
 
-application()
+app()

0495b53cfcfac6563064a9bd06527cffcda4645d
JiangShaoYin <tooo_cold@163.com>
2018-11-14 19:50:08 +0800

    auto

--- Mnist/my_mnist/mnist_label.py
+++ Mnist/my_mnist/mnist_label.py
@@ -9,15 +9,22 @@ import tensorflow as tf
 import numpy as np
 import os
 
-train_folder = ~/AI_Practice/Mnist/my_mnist/cifar-10/train
-test_folder = ~/AI_Practice/Mnist/my_mnist/cifar-10/train
+train_folder = '/home/jiang/AI_Practice/Mnist/my_mnist/cifar-10/train'
+test_folder = '~/AI_Practice/Mnist/my_mnist/cifar-10/test'
 
 
 
-
-def ReadFile(dir):
-    pic_folder = os.listdir(dir_file)
+def ReadFile(path):
+    pic_folder = os.listdir(path)
+    for folder in pic_folder:
+        pic_folder_path = path + "/" + folder
+        files = os.listdir(pic_folder_path)
+        train_dataset_label = open((path + '.txt'),'w')
     print pic_folder
 
-def WriteFile()
+def WriteFile():
+    print "helloworld"
+
+
+ReadFile(train_folder)
 

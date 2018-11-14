d5003f13803bb3a0d04da188058399a8a5b3ceb4
JiangShaoYin <tooo_cold@163.com>
2018-11-14 18:56:09 +0800

    auto

---
+++ Mnist/my_mnist/mnist_label.py
@@ -0,0 +1,23 @@
+ 
+#        coding:utf-8
+#	 @file    mnist_label.py
+#	 @author  1801210547_江绍印(jiangshaoyin@pku.edu.cn)
+#	 @date    2018-11-14 18:36:02
+ 
+ 
+import tensorflow as tf
+import numpy as np
+import os
+
+train_folder = ~/AI_Practice/Mnist/my_mnist/cifar-10/train
+test_folder = ~/AI_Practice/Mnist/my_mnist/cifar-10/train
+
+
+
+
+def ReadFile(dir):
+    pic_folder = os.listdir(dir_file)
+    print pic_folder
+
+def WriteFile()
+

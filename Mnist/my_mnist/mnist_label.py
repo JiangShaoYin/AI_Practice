6bb93cf06915a345180fb9714c2297e8751c6413
JiangShaoYin <tooo_cold@163.com>
2018-11-14 20:55:00 +0800

    auto

--- Mnist/my_mnist/mnist_label.py
+++ Mnist/my_mnist/mnist_label.py
@@ -1,30 +1,37 @@
- 
 #        coding:utf-8
 #	 @file    mnist_label.py
 #	 @author  1801210547_江绍印(jiangshaoyin@pku.edu.cn)
 #	 @date    2018-11-14 18:36:02
- 
  
 import tensorflow as tf
 import numpy as np
 import os
 
 train_folder = '/home/jiang/AI_Practice/Mnist/my_mnist/cifar-10/train'
-test_folder = '~/AI_Practice/Mnist/my_mnist/cifar-10/test'
-
-
+test_folder = '/home/jiang/AI_Practice/Mnist/my_mnist/cifar-10/test'
 
 def ReadFile(path):
-    pic_folder = os.listdir(path)
-    for folder in pic_folder:
+    pic_folder = os.listdir(path) #pic_folder is a list which equals to  [bird, dog, ....]
+
+    dict_pic_num ={'airplane':0, 'automobile':1, 'bird':2, 'cat':3, 'deer':4,
+                   'dog':5, 'frog':6, 'horse':7, 'ship':8, 'truck':9}
+
+    train_dataset_label = open((path + '_label.txt'),'w')
+
+    for folder in pic_folder: # folder like fog ,cat ,etc 
         pic_folder_path = path + "/" + folder
+        #open folder in ./train, such as frog cat bird etc
         files = os.listdir(pic_folder_path)
-        train_dataset_label = open((path + '.txt'),'w')
+        
+        for filename in files:#filename == batch_2_num_6379.jpg
+            train_dataset_label.write(filename + '_' + folder + ' ' + str(dict_pic_num[folder]) +'\n')
+
     print pic_folder
 
 def WriteFile():
-    print "helloworld"
+   print "helloworld"
 
 
 ReadFile(train_folder)
+ReadFile(test_folder)
 
